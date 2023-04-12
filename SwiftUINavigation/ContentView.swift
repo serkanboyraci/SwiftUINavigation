8bdb0b585d8f4af3ec63b678dad3e6ce8fc90455
serkanboyraci <serkanboyraci@gmail.com>
2023-04-12 13:28:38 +0200

    NavigationStack and Destination added

--- SwiftUINavigation/ContentView.swift
+++ SwiftUINavigation/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            List(dogs) {dog in
-                NavigationLink {
-                    SecondView(selectedDog: dog)
-                } label: {
-                    Text(dog.name)
+        NavigationStack{
+            
+            List {
+                Section("Dogs") {
+                    ForEach(dogs) { dog in
+                        NavigationLink(dog.name, value: dog)
+                    }
                 }
+                        
+                Section("Cats") {
+                    ForEach(cats) { cat in
+                        NavigationLink(cat.name, value: cat)
+                    }
+                }
+            }.navigationDestination(for: Dog.self) { dog in
+                Text(dog.name)
+            }
+            .navigationDestination(for: Cat.self) { cat in
+                Text(cat.name)
             }
         }
     }
