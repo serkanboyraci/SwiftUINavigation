a26168dfac8abec3518e74576d2c0897a3c19f82
serkanboyraci <serkanboyraci@gmail.com>
2023-04-12 12:11:56 +0200

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Ali serkan Boyracı  on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
